cff362f164a212f3faeac5bad998fab9f8b7e4f8
Tomoki Kobayashi <temoki@gmail.com>
2025-06-26 21:43:07 +0900

    Initial Commit

---
+++ lq2/lq2App.swift
@@ -0,0 +1,17 @@
+//
+//  lq2App.swift
+//  lq2
+//
+//  Created by Tomoki Kobayashi on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct lq2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
